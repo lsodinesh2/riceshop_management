86a93cfdd2ab817acfeae99f9e89916bf101e4a3
lsodinesh2 <lsodinesh2@gmail.com>
2026-06-07 16:25:39 +0530

    Fixed Railway database connection

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
-import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host=os.environ.get("mysql.railway.internal"),
-        user=os.environ.get("root"),
-        password=os.environ.get("EEnIJRGNIZJpQeBEtXlvcVnllyflqhmR"),
-        database=os.environ.get("railway"),
-        port=int(os.environ.get("3306", 3306))
+        host="mysql.railway.internal",
+        user="root",
+        password="EEnIJRGNIZJpQeBEtXlvcVnllyflqhmR",
+        database="railway",
+        port=3306
     )
